848dd616d281f9ecbd0cb3b89463bceb5388e9eb
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-01-05 08:59:11 +0900

    나이 출력

--- src/codekata/solution2.java
+++ src/codekata/solution2.java
@@ -1,4 +1,12 @@
 package codekata;
 
-public class solution2 {
+class Solution2 {
+    public int solution2(int age) {
+        int answer = 0;
+        int year = 2023;
+
+        answer = year - age;
+
+        return answer;
+    }
 }
