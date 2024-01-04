84d694d0e0850bed3232d77c5daf8cde2099326d
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-01-04 10:38:35 +0900

    두 수의 곱

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,19 @@
-public class Solution {
+
+// 두 수의 차
+//public class Solution {
+//    public int solution(int num1, int num2) {
+//        return num1 - num2;
+//    }
+//}
+
+// 두 수의 곱
+class Solution {
     public int solution(int num1, int num2) {
-        return num1 - num2;
+        boolean val = (0<=num1 && num1 <=100 && 0<=num2 &&num2<=100);
+        int answer = 0;
+        if(val){
+            answer = num1 *num2;
+        }
+        return answer;
     }
 }
