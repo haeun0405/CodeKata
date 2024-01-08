3b158b66b4ac7850c262a8bb997edef49fdf31f6
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-01-08 09:17:57 +0900

    숫자 비교하기

---
+++ src/codekata/Solution3.java
@@ -0,0 +1,13 @@
+package codekata;
+
+class Solution3 {
+    public int solution(int num1, int num2) {
+        boolean val = (0<=num1 && num1<=10000 && 0<=num2 && num2 <=10000);
+        int answer = 0;
+
+        if(val){
+            answer=(num1==num2)? 1: -1;
+        }
+        return answer;
+    }
+}
