1b2b6e01010c8909892a14873375d0e278b18cad
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-01-16 09:27:56 +0900

    자연수 뒤집어 배열로 만들기

--- src/codekata/Solution3.java
+++ src/codekata/Solution3.java
@@ -149,15 +149,29 @@ class Solution3 {
 //        }
 
     // x만큼 간격이 있는 n개의 숫자
+//    static class Solution {
+//        public long[] solution(int x, int n) {
+//            long[] answer = new long[n];
+//            long sum = 0;
+//            for(int i = 0;i<answer.length;i++){
+//                sum += x;
+//                answer[i] = sum;
+//            }
+//
+//
+//            return answer;
+//        }
+//    }
+    // 자연수 뒤집어 배열로 만들기
     static class Solution {
-        public long[] solution(int x, int n) {
-            long[] answer = new long[n];
-            long sum = 0;
-            for(int i = 0;i<answer.length;i++){
-                sum += x;
-                answer[i] = sum;
-            }
+        public int[] solution(long n) {
+            int length = Long.toString(n).length();
+            int[] answer = new int[length];
 
+            for (int i = 0; i < length; i++) {
+                answer[i] = (int) (n % 10);
+                n /= 10;
+            }
 
             return answer;
         }
