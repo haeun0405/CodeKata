c4bb4ef17f8625c0fbd68d30023ebcc424d218ab
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-01-16 09:56:23 +0900

    문자열을 정수로 바꾸기

--- src/codekata/Solution3.java
+++ src/codekata/Solution3.java
@@ -163,19 +163,27 @@ class Solution3 {
 //        }
 //    }
     // 자연수 뒤집어 배열로 만들기
+//    static class Solution {
+//        public int[] solution(long n) {
+//            int length = Long.toString(n).length();
+//            int[] answer = new int[length];
+//
+//            for (int i = 0; i < length; i++) {
+//                answer[i] = (int) (n % 10);
+//                n /= 10;
+//            }
+//
+//            return answer;
+//        }
+//    }
+    // 문자열을 정수로 바꾸기
     static class Solution {
-        public int[] solution(long n) {
-            int length = Long.toString(n).length();
-            int[] answer = new int[length];
-
-            for (int i = 0; i < length; i++) {
-                answer[i] = (int) (n % 10);
-                n /= 10;
-            }
-
-            return answer;
+        public int solution(String s) {
+            String answer = s;
+            return Integer.parseInt(answer);
         }
     }
+
 }
 
 
